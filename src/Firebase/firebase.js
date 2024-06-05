50871c22409b89de3c1939e443940a41e5d89944
Saurav Antony <145794226+Roger5791@users.noreply.github.com>
2024-06-05 13:14:44 +0530

    Update firebase.js

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM1J4Sqn1dWl8CRZVJkSZhUVPmHexhskc",
-  authDomain: "e-store-c2f27.firebaseapp.com",
-  projectId: "e-store-c2f27",
-  storageBucket: "e-store-c2f27.appspot.com",
-  messagingSenderId: "720702759786",
-  appId: "1:720702759786:web:c62c6b9d214f01b2d8fb13"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "m",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
